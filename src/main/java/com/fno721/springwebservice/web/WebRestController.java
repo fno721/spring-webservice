c8e9c4b854ed40bec4f2a851164f94317a02fe86
fno721 <ghqkaqka325@gmail.com>
2018-05-04 12:04:32 -0400

    v프로젝트 생성

---
+++ src/main/java/com/fno721/springwebservice/web/WebRestController.java
@@ -0,0 +1,13 @@
+package com.fno721.springwebservice.web;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WebRestController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "HelloWorld";
+    }
+}
